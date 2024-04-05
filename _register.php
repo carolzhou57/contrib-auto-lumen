de5b2170dde9bc93896eb5f0f73008fd061b806e
dkudrin <dkudrin@jmart.kz>
2024-04-05 11:38:05 +0500

    changes for Lumen support

--- _register.php
+++ _register.php
@@ -15,18 +15,4 @@ if (extension_loaded('opentelemetry') === false) {
     return;
 }
 
-$jaegerHost = env('JAEGER_HOST', 'http://localhost:4318');
-putenv('OTEL_SDK_DISABLED=false');
-putenv('OTEL_SERVICE_NAME=' . env('APP_NAME', 'lumen'));
-putenv('OTEL_PHP_AUTOLOAD_ENABLED=true');
-putenv('OTEL_TRACES_EXPORTER=otlp');
-putenv('OTEL_METRICS_EXPORTER=none');
-putenv('OTEL_LOGS_EXPORTER=none');
-putenv('OTEL_EXPORTER_OTLP_ENDPOINT=' . $jaegerHost);
-putenv('OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=' . $jaegerHost . '/v1/traces');
-putenv('OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf');
-putenv('OTEL_PROPAGATORS=baggage,tracecontext');
-putenv('OTEL_TRACES_SAMPLER=parentbased_always_on');
-putenv('OTEL_PHP_DETECTORS=all');
-
 LumenInstrumentation::register();
